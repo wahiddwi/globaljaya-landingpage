90e0d782c789466871f57a84fa288481c2d5240d
wahiddwi <wahiddwi7@gmail.com>
2021-12-06 23:22:43 +0700

    rapi-rapi

--- resources/views/pages/admin/lokasi/edit.blade.php
+++ resources/views/pages/admin/lokasi/edit.blade.php
@@ -49,7 +49,7 @@
                             <div class="form-group col-md-6">
                                 <label for="inputAddress2">Latitude</label>
                                 <input type="text" class="form-control @error('latitude') is-invalid @enderror"
-                                    name="latitude" id="inputAddress2" value="{{ $lokasi->latitude }}" placeholder="Latitude" required readonly>
+                                    name="latitude" id="inputAddress2" value="{{ $lokasi->latitude }}" placeholder="Latitude" required>
                                     @error('latitude')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -60,7 +60,7 @@
                             <div class="form-group col-md-6">
                                 <label for="inputAddress2">Longitude</label>
                                 <input type="text" class="form-control @error('longitude') is-invalid @enderror"
-                                    name="longitude" id="inputAddress2" value="{{ $lokasi->longitude }}" placeholder="Longitude" required readonly>
+                                    name="longitude" id="inputAddress2" value="{{ $lokasi->longitude }}" placeholder="Longitude" required>
                                     @error('longitude')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
